00eda128a39175219b881c1adac1a332313a0418
defgao <33217367+defgao@users.noreply.github.com>
2020-01-18 10:16:50 +0800

    fifth commit

---
+++ service/file.js
@@ -0,0 +1,10 @@
+const db = require('../models/index');
+const  {File} = db.models;
+//文件夹默认是私有的 文件有公开的 广场上只有文件 没有文件夹 广场上的允许设个验证码分享
+async function addFile( ){
+
+};
+let fileHandle = {
+
+};
+module.exports = fileHandle;
--- service/user.js
+++ service/user.js
@@ -6,8 +6,37 @@ async function addUser(name , password){
         name: name,
         password: password
     })
-};
+}
+async function findUser(name , password){
+    await User.findOne({
+        where:{
+            name : name,
+            password : password
+        }
+    }).then(data => {
+        return data === undefined;
+    });
+}
+async function updateUser_info(name , password , newname , newpassword){
+    let id = await User.findOne({
+        attributes:['id'],
+        where:{
+            name : name ,
+            password : password
+        }
+    });
+    await User.update({
+        name,
+        password
+    }, {
+        where:{
+            id : id
+        }
+    });
+}
 let userHandle = {
-    addUser:addUser
+    addUser: addUser,
+    checkIn: findUser,
+    updateUser_info : updateUser_info
 };
 module.exports = userHandle;
